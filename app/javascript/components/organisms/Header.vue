972a28789544a01aec79cfab31a5edc699813e94
kthatoto <kthatoto@gmail.com>
2018-07-17 22:06:05 +0900

    Added header

---
+++ app/javascript/components/organisms/Header.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+.header
+</template>
+<script>
+export default {
+  props: ['user']
+}
+</script>
+<style lang="scss">
+.header {
+  width: 100%;
+  background-color: #333;
+  height: 80px;
+}
+</style>
--- app/javascript/pages/TasksDashboard.vue
+++ app/javascript/pages/TasksDashboard.vue
@@ -1,16 +1,17 @@
 <template lang="pug">
 .tasksDashboard
+  Header
+  p(v-for="task in tasks") {{ task }}
 </template>
 
 <script>
+import Header from '../components/organisms/Header'
 export default {
+  components: { Header },
   props: ['tasks'],
   data () {
     return {
     }
-  },
-  created () {
-    console.log(this.tasks)
   }
 }
 </script>
