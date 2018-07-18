3fd1f6c5db981c5f5135d119d2437d76f76fe4d5
kthatoto <kthatoto@gmail.com>
2018-07-18 16:50:42 +0900

    Edited devise route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get    'signin',  to: 'devise/sessions#new'
+    get    'signup',  to: 'devise/registrations#new'
+    delete 'signout', to: 'devise/sessions#destroy'
+  end
   root "pages#tasks_dashboard"
   get 'users', to: 'pages#user_list'
 end
