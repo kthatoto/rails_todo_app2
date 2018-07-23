acdb898ac5cbe215d844c8a9f735ebf0a0a9223f
kthatoto <kthatoto@gmail.com>
2018-07-23 17:55:38 +0900

    Added api routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
     get    'signup',  to: 'devise/registrations#new'
     delete 'signout', to: 'devise/sessions#destroy'
   end
-  root "pages#tasks_dashboard"
+  root 'pages#tasks_dashboard'
   get 'users', to: 'pages#user_list'
+
+  namespace :api do
+    resources :tasks, only: [:create, :update, :destroy]
+    scope '/search' do
+      get 'users', to: 'users#search'
+      get 'labels', to: 'labels#search'
+    end
+  end
 end
