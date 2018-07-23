f8bb1151db2e2b7f0bfd676313ff4e1f390b926c
kthatoto <kthatoto@gmail.com>
2018-07-23 19:09:41 +0900

    Use task list in page component

--- app/javascript/pages/TasksDashboard.vue
+++ app/javascript/pages/TasksDashboard.vue
@@ -1,9 +1,13 @@
 <template lang="pug">
 .tasksDashboard
+  task-list(header="TODO" :tasks="props.todos")
+  task-list(header="DONE" :tasks="props.dones")
 </template>
 
 <script>
+import TaskList from '@/components/organisms/TaskList'
 export default {
+  components: { TaskList },
   props: ['props'],
   data () {
     return {
