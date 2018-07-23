1c92ff574b4a01d5132d030470476d4f1a16c73c
kthatoto <kthatoto@gmail.com>
2018-07-23 18:21:34 +0900

    Added user and task seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
-User.create(name: 'kthatoto', password: 'password')
+user = User.create(name: 'kthatoto', password: 'password')
+6.times do |i|
+  task = Task.create(
+    content: "Content #{i + 1}",
+    status: i % 2,
+  )
+  Assignment.create(
+    task_id: task.id,
+    user_id: user.id
+  )
+end
