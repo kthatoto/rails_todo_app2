83782cd0bb352731508833af5743ffe18b3debd2
kthatoto <kthatoto@gmail.com>
2018-07-23 22:36:13 +0900

    Added buttons to task console

--- app/javascript/pages/TasksDashboard.vue
+++ app/javascript/pages/TasksDashboard.vue
@@ -1,25 +1,34 @@
 <template lang="pug">
 .tasksDashboard
+  .taskConsole
+    .taskConsole__buttons
+      Button(body="Create New Task")
+      Button(body="Filter Tasks")
   .taskLists
     task-list.taskList(type="todo" :tasks="props.todos")
     task-list.taskList(type="done" :tasks="props.dones")
 </template>
 
 <script>
+import Button from '@/components/atoms/Button'
 import TaskList from '@/components/organisms/TaskList'
 export default {
-  components: { TaskList },
+  components: { Button, TaskList },
   props: ['props']
 }
 </script>
 
 <style lang="scss" scoped>
 .tasksDashboard {
-}
-.taskLists {
+  padding-top: 30px;
   margin: auto;
   max-width: 900px;
   min-width: 650px;
+}
+.taskConsole {
+  margin-bottom: 30px;
+}
+.taskLists {
   display: flex;
 }
 .taskList {
