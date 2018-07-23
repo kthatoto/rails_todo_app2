22e99fba783da58364a3330dee989eb9117d0e2c
kthatoto <kthatoto@gmail.com>
2018-07-23 18:06:22 +0900

    Callback exec sample with button

---
+++ app/javascript/components/atoms/Button.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  .button(@click="proc") {{ label }}
+</template>
+<script>
+export default {
+  props: ['label', 'proc']
+}
+</script>
+<style lang="scss" scoped>
+.button {
+  width: 100px;
+  height: 40px;
+  border: 1px solid black;
+}
+</style>
--- app/javascript/pages/TasksDashboard.vue
+++ app/javascript/pages/TasksDashboard.vue
@@ -1,12 +1,24 @@
 <template lang="pug">
 .tasksDashboard
+  Button(label="ok!" :proc="alertMsg(msg)")
+  input(type=text v-model="msg")
 </template>
 
 <script>
+import Button from '@/components/atoms/Button'
 export default {
   props: ['props'],
+  components: { Button },
   data () {
     return {
+      msg: ''
+    }
+  },
+  methods: {
+    alertMsg (msg) {
+      return () => {
+        alert(msg)
+      }
     }
   }
 }
