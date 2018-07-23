7ed880df95589179c3becb6e59c601748a39d387
kthatoto <kthatoto@gmail.com>
2018-07-23 19:10:19 +0900

    Added task list component

---
+++ app/javascript/components/organisms/TaskList.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+.taskList
+  h2.taskList__header {{ header }}
+  ul.taskList__list
+    li(v-for="task in tasks" :key="task.id") {{ task.content }}
+</template>
+<script>
+export default {
+  props: ['header', 'tasks']
+}
+</script>
+<style lang="scss" scoped>
+.taskList {
+}
+</style>
