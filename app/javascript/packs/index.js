3054d50458be98390abe63f5b2c278c487818502
kthatoto <kthatoto@gmail.com>
2018-07-24 23:12:47 +0900

    Import store

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue/dist/vue.esm'
+import store from '@/vuex/store'
 import ComponentWrapper from './ComponentWrapper'
 import VueHeader from '@/components/organisms/Header'
 import 'vue-awesome/icons'
 import Icon from 'vue-awesome/components/Icon'
 Vue.component('icon', Icon)
+
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
-    components: { ComponentWrapper, VueHeader }
+    components: { ComponentWrapper, VueHeader },
+    store
   }).$mount('#app')
 })
