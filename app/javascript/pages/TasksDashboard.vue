d0e55113b80b437ac232288e2ce495597f639655
kthatoto <kthatoto@gmail.com>
2018-07-24 22:42:36 +0900

    Added new task form

--- app/javascript/pages/TasksDashboard.vue
+++ app/javascript/pages/TasksDashboard.vue
@@ -2,23 +2,39 @@
 .tasksDashboard
   .taskConsole
     .taskConsole__buttons
-      Button.button
+      Button.button(:proc="showNewTaskForm()")
         icon.icon.-left(name="plus")
         | Create New Task
       Button.button
         icon.icon.-left(name="search")
         | Filter Tasks
+    task-form.newTaskForm(v-show="showing.newTaskForm")
   .taskLists
     task-list.taskList(type="todo" :tasks="props.todos")
     task-list.taskList(type="done" :tasks="props.dones")
 </template>
 
 <script>
-import Button from '@/components/atoms/Button'
+import NewTaskForm from '@/components/organisms/taskForm/NewTaskForm'
 import TaskList from '@/components/organisms/TaskList'
+import Button from '@/components/atoms/Button'
 export default {
-  components: { Button, TaskList },
-  props: ['props']
+  components: { TaskForm, TaskList, Button },
+  props: ['props'],
+  data () {
+    return {
+      showing: {
+        newTaskForm: true
+      }
+    }
+  },
+  methods: {
+    showNewTaskForm () {
+      return () => {
+        this.showing.newTaskForm = true
+      }
+    }
+  }
 }
 </script>
 
@@ -30,8 +46,9 @@ export default {
   min-width: 650px;
 }
 .taskConsole {
-  margin-bottom: 30px;
+  margin-bottom: 20px;
   &__buttons {
+    margin-bottom: 10px;
     .button:nth-child(2) {
       margin-left: 5px;
     }
