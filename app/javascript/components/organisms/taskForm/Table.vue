606752bcbdb7ecb9c7648f8aedb303e289df9cd7
kthatoto <kthatoto@gmail.com>
2018-07-24 22:42:54 +0900

    Added new task form components

---
+++ app/javascript/components/organisms/taskForm/Table.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+table.table
+  tr
+    th Content
+    td
+      textarea
+  tr
+    th Assignees
+    td
+      input
+  tr
+    th Labels
+    td
+      input
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="scss">
+.table {
+}
+</style>
