e3ff431750041a8234c8d1aac3bdf3da764f754b
kthatoto <kthatoto@gmail.com>
2018-07-28 20:29:32 +0900

    Addedd label form

---
+++ app/javascript/components/molecules/taskFormRows/Labels.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+tr
+  th Labels
+  td
+    input(type=text v-model="labelName" list="labelList" @keydown="addLabel")
+    datalist(id="labelList")
+      option(v-for="label in labels" :key="label.id" :value="label.name")
+    ul
+      li(v-for="label in addingLabels" :key="label.id") {{ label.name }}
+</template>
+<script>
+import store from '@/vuex/store'
+export default {
+  data () {
+    return {
+      labelName: '',
+      labels: [],
+      addingLabels: []
+    }
+  },
+  created () {
+    // this.labels = store.getters.getLabels
+  },
+  methods: {
+    addLabel (e) {
+      if (!(e.keyCode === 13) || this.labelName.length === 0) {
+        return
+      }
+      if (this.addingLabels.filter(label => label === this.labelName).length > 0) {
+        alert('Already added')
+        return
+      }
+      this.addingLabels.push(this.labelName)
+      this.labelName = ''
+    }
+  },
+  watch: {
+    assignees () {
+      this.$emit('update', this.assignees.map(assignee => assignee.id))
+    }
+  }
+}
+</script>
+<style lang="scss">
+</style>
--- app/javascript/components/organisms/taskForms/NewTaskForm.vue
+++ app/javascript/components/organisms/taskForms/NewTaskForm.vue
@@ -4,13 +4,15 @@ task-form.newTaskForm
   table
     content-row(@update="newContent => content = newContent")
     assignees-row(@update="newIds => assigneeIds = newIds")
+    labels-row(@update="newLabels => labels = newLabels")
 </template>
 <script>
 import TaskForm from '@/components/molecules/Form'
 import ContentRow from '@/components/molecules/taskFormRows/Content'
 import AssigneesRow from '@/components/molecules/taskFormRows/Assignees'
+import LabelsRow from '@/components/molecules/taskFormRows/Labels'
 export default {
-  components: { TaskForm, ContentRow, AssigneesRow },
+  components: { TaskForm, ContentRow, AssigneesRow, LabelsRow },
   data () {
     return {
       content: '',
