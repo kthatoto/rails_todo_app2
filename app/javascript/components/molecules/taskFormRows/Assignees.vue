04af9cc554d599e695dc346e31210cd1cacc3f05
kthatoto <kthatoto@gmail.com>
2018-07-28 22:40:21 +0900

    Added scoped

--- app/javascript/components/molecules/taskFormRows/Assignees.vue
+++ app/javascript/components/molecules/taskFormRows/Assignees.vue
@@ -51,7 +51,7 @@ export default {
   }
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 .assignees {
   &__item {
     display: inline-block;
--- app/javascript/components/molecules/taskFormRows/Labels.vue
+++ app/javascript/components/molecules/taskFormRows/Labels.vue
@@ -47,7 +47,7 @@ export default {
   }
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 .labels {
   &__item {
     display: inline-block;
