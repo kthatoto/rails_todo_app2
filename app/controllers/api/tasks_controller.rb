c670b10b7c52aa45c5313a9d45ac7afaa1e0a768
kthatoto <kthatoto@gmail.com>
2018-07-29 00:51:05 +0900

    Added task update logic

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -11,9 +11,34 @@ class Api::TasksController < ApplicationController
         label = Label.find_or_create_by(name: label_name)
         TaskLabel.create!(task_id: task.id, label_id: label.id)
       end
-      render json: {task: task}, status: 201
+      render json: {task: task.json}, status: 201
     else
       render json: {error: :create_failed}, status: 400
+    end
+  end
+  def update
+    task = Task.find(params[:id])
+    if task.update(task_params)
+      if params[:assigneeIds]
+        task.assignments.where.not(user_id: params[:assigneeIds]).destroy_all
+        params[:assigneeIds].each do |assignee_id|
+          User.find(assignee_id)
+          Assignment.find_or_create_by(task_id: task.id, user_id: assignee_id)
+        end
+      end
+      if params[:labels]
+        TaskLabel.joins(:label, :task)
+          .where(tasks: {id: task.id})
+          .where.not(labels: {name: params[:labels]})
+          .destroy_all
+        params[:labels].each do |label_name|
+          label = Label.find_or_create_by(name: label_name)
+          TaskLabel.find_or_create_by(task_id: task.id, label_id: label.id)
+        end
+      end
+      render json: {task: task.json}, status: 200
+    else
+      render json: {error: :update_failed}, status: 400
     end
   end
   private
