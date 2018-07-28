dce2e50a91c86d1cc8e3e0e6ebe76c1e512fad8a
kthatoto <kthatoto@gmail.com>
2018-07-28 19:35:00 +0900

    Added assignees row

---
+++ app/javascript/components/molecules/taskFormRows/Assignees.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+tr
+  th Assignees
+  td
+    input(type=text v-model="assigneeName" list="assigneeList")
+    datalist(id="assigneeList")
+      option(value="JavaScript")
+      option(value="Java")
+      option(value="PHP")
+      option(value="Python")
+    ul
+      li(v-for="assignee in assignees" :key="assignee.id")
+</template>
+<script>
+export default {
+  data () {
+    return {
+      assignees: [],
+      assigneeName: ''
+    }
+  }
+}
+</script>
+<style lang="scss">
+</style>
--- app/javascript/components/organisms/taskForms/NewTaskForm.vue
+++ app/javascript/components/organisms/taskForms/NewTaskForm.vue
@@ -3,12 +3,14 @@ task-form.newTaskForm
   h2 New Task
   table
     content-row(@update="newContent => content = newContent")
+    assignees-row(@update="newIds => assigneeIds = newIds")
 </template>
 <script>
 import TaskForm from '@/components/molecules/Form'
 import ContentRow from '@/components/molecules/taskFormRows/Content'
+import AssigneesRow from '@/components/molecules/taskFormRows/Assignees'
 export default {
-  components: { TaskForm, ContentRow },
+  components: { TaskForm, ContentRow, AssigneesRow },
   data () {
     return {
       content: '',
