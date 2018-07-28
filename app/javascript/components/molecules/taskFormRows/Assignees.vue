dd6880ac842e97f857a6f169f1060f64ef6993b6
kthatoto <kthatoto@gmail.com>
2018-07-28 19:54:12 +0900

    Added assignees

--- app/javascript/components/molecules/taskFormRows/Assignees.vue
+++ app/javascript/components/molecules/taskFormRows/Assignees.vue
@@ -2,21 +2,46 @@
 tr
   th Assignees
   td
-    input(type=text v-model="assigneeName" list="assigneeList")
+    input(type=text v-model="assigneeName" list="assigneeList" @keydown="addAssignee")
     datalist(id="assigneeList")
-      option(value="JavaScript")
-      option(value="Java")
-      option(value="PHP")
-      option(value="Python")
+      option(v-for="user in users" :key="user.id" :value="user.name")
     ul
-      li(v-for="assignee in assignees" :key="assignee.id")
+      li(v-for="assignee in assignees" :key="assignee.id") {{ assignee.name }}
 </template>
 <script>
+import store from '@/vuex/store'
 export default {
   data () {
     return {
+      users: [],
       assignees: [],
       assigneeName: ''
+    }
+  },
+  created () {
+    this.users = store.getters.getUsers
+  },
+  methods: {
+    addAssignee (e) {
+      if (!(e.keyCode === 13) || this.assigneeName.length === 0) {
+        return
+      }
+      const targetUser = this.users.filter(user => user.name === this.assigneeName)[0]
+      if (!targetUser) {
+        alert('User not found')
+        return
+      }
+      if (this.assignees.filter(assignee => assignee.id === targetUser.id).length > 0) {
+        alert('Already added')
+        return
+      }
+      this.assignees.push(targetUser)
+      this.assigneeName = ''
+    }
+  },
+  watch: {
+    assignees () {
+      this.$emit('update', this.assignees.map(assignee => assignee.id))
     }
   }
 }
